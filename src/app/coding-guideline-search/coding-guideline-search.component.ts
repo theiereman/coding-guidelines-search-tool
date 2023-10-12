6f4644213560e2a9e3683166c27eb98efe890d49
Thomas EYERMANN <thomas.eyermann@custy.com>
2023-10-12 16:28:33 +0200

    init

---
+++ src/app/coding-guideline-search/coding-guideline-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coding-guideline-search',
+  templateUrl: './coding-guideline-search.component.html',
+  styleUrls: ['./coding-guideline-search.component.css']
+})
+export class CodingGuidelineSearchComponent {
+
+}
