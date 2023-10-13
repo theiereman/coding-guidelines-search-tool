e4729cfdafd0aba81a38571269dd36468b2ce7a2
Thomas Eyermann <thomas.eyermann@protonmail.com>
2023-10-13 22:37:03 +0200

    connexion basique avec récupération des informations de l'utilisateur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { MsalService, MsalBroadcastService } from '@azure/msal-angular';
-import { AuthenticationResult, InteractionStatus, PopupRequest, RedirectRequest, EventMessage, EventType } from '@azure/msal-browser';
-import { Subject } from 'rxjs';
-import { filter, takeUntil } from 'rxjs/operators';
-
-
-import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
+import { User } from './user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,72 +10,32 @@ import { environment } from 'src/environments/environment';
 })
 export class AppComponent {
   title = 'Charte de programmation - Outil de recherche';
+  user?:User;
   loginDisplay = false;
-  private readonly _destroying$ = new Subject<void>();
 
-  constructor(
-    private authService: MsalService,
-    private msalBroadcastService: MsalBroadcastService,
-  ) {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-
-    this.authService.handleRedirectObservable().subscribe();
-
+    this.authService.handleRedirects().subscribe();
     this.setLoginDisplay();
-    this.authService.instance.enableAccountStorageEvents(); // Optional - This will enable ACCOUNT_ADDED and ACCOUNT_REMOVED events emitted when a user logs in or out of another tab or window
-    
-    this.msalBroadcastService.msalSubject$
-      .pipe(
-        filter((msg: EventMessage) => msg.eventType === EventType.ACCOUNT_ADDED || msg.eventType === EventType.ACCOUNT_REMOVED),
-      )
-      .subscribe((result: EventMessage) => {
-        if (this.authService.instance.getAllAccounts().length === 0) {
-          window.location.pathname = "/";
-        } else {
-          this.setLoginDisplay();
-        }
-      });
-    
-    this.msalBroadcastService.inProgress$
-      .pipe(
-        filter((status: InteractionStatus) => status === InteractionStatus.None),
-        takeUntil(this._destroying$)
-      )
-      .subscribe(() => {
-        this.setLoginDisplay();
-        this.checkAndSetActiveAccount();
-      })
+    this.authService.getUserObservable().subscribe(user => {
+      this.user = user;
+    })
   }
 
   login(): void {
-    this.authService.loginRedirect();
+    this.authService.login().subscribe(_ => {
+      this.setLoginDisplay()
+    });
   }
 
   logout() {
-    this.authService.logoutRedirect();
+    this.authService.logout().subscribe(_ => {
+      this.setLoginDisplay()
+    });
   }
 
   setLoginDisplay() {
-    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
-  }
-
-  checkAndSetActiveAccount(){
-    /**
-     * If no active account set but there are accounts signed in, sets first account to active account
-     * To use active account set here, subscribe to inProgress$ first in your component
-     * Note: Basic usage demonstrated. Your app may require more complicated account selection logic
-     */
-    let activeAccount = this.authService.instance.getActiveAccount();
-
-    if (!activeAccount && this.authService.instance.getAllAccounts().length > 0) {
-      let accounts = this.authService.instance.getAllAccounts();
-      this.authService.instance.setActiveAccount(accounts[0]);
-    }
-  }
-
-  ngOnDestroy(): void {
-    this._destroying$.next(undefined);
-    this._destroying$.complete();
+    this.loginDisplay = this.authService.isAuthenticated();
   }
 }
---
+++ src/app/auth.service.ts
@@ -0,0 +1,110 @@
+import { Injectable } from '@angular/core';
+import { MsalService, MsalBroadcastService } from '@azure/msal-angular';
+import {AuthenticationResult,InteractionStatus, PublicClientApplication, InteractionType} from '@azure/msal-browser';
+import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
+import { AuthCodeMSALBrowserAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser';
+import { Client } from '@microsoft/microsoft-graph-client';
+import { Observable,Subject, tap, filter, takeUntil, of, from, map} from 'rxjs';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  public graphClient?: Client;
+  private userSubject = new Subject<User | undefined>();
+  private readonly _destroying$ = new Subject<void>();
+
+  constructor(
+    private msalService: MsalService,
+    private msalBroadcastService: MsalBroadcastService
+  ) {
+    //vérifie le statut de interactions 
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this._destroying$)
+      )
+      .subscribe(() => {
+        this.checkAndSetActiveAccount();
+        this.getUser().subscribe((user:User | undefined) => {
+          console.log(JSON.stringify(user));
+        });
+      })
+  }
+
+  getUserObservable(): Observable<User | undefined> {
+    return this.userSubject.asObservable();
+  }
+
+  login(): Observable<AuthenticationResult> {
+    return this.msalService.loginPopup();
+  }
+
+  logout(): Observable<void> {
+    return this.msalService.logoutPopup()
+    .pipe(
+      tap(_ => {
+        this.graphClient = undefined;
+        this.userSubject.next(undefined); 
+      })
+    );
+  }
+
+  isAuthenticated(): boolean {
+    return this.msalService.instance.getAllAccounts().length > 0;
+  }
+
+  handleRedirects(): Observable<AuthenticationResult> {
+    return this.msalService.handleRedirectObservable();
+  }
+
+  private checkAndSetActiveAccount(){
+    let activeAccount = this.msalService.instance.getActiveAccount();
+
+    if (!activeAccount && this.msalService.instance.getAllAccounts().length > 0) {
+      let accounts = this.msalService.instance.getAllAccounts();
+      this.msalService.instance.setActiveAccount(accounts[0]);
+    }
+  }
+  
+  private getUser(): Observable<User | undefined> {
+    if (!this.isAuthenticated()) return of(undefined);
+
+    // Create an authentication provider for the current user
+    const authProvider = new AuthCodeMSALBrowserAuthenticationProvider(
+      this.msalService.instance as PublicClientApplication,
+      {
+        account: this.msalService.instance.getActiveAccount()!,
+        scopes: ['user.read', 'mailboxsettings.read', 'calendars.readwrite'],
+        interactionType: InteractionType.Popup,
+      },
+    );
+
+    // Initialize the Graph client
+    this.graphClient = Client.initWithMiddleware({
+      authProvider: authProvider,
+    });
+
+    console.log("debug")
+
+    // Get the user from Graph (GET /me)
+    let res:Observable<User> = from(this.graphClient.api('/me').select('displayName,mail,mailboxSettings,userPrincipalName').get())
+                    .pipe(map((res) => {
+                      console.log(res);
+                      const resUser:User = {
+                        displayName: res.displayName ?? '',
+                        email: res.mail ?? res.userPrincipalName ?? ''
+                      }
+                      this.userSubject.next(resUser); 
+                      return resUser
+                    }));
+
+    return res;
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
+}
