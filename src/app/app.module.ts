85d26c214aea1f567f4f439bdb9520e074e079b5
Thomas Eyermann <thomas.eyermann@protonmail.com>
2023-10-13 18:38:27 +0200

    msal authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,17 @@ import { AppComponent } from './app.component';
 import { CodingGuidelineSearchComponent } from './coding-guideline-search/coding-guideline-search.component';
 import { FormsModule } from '@angular/forms';
 
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MsalModule, MsalService, MsalGuard, MsalInterceptor, MsalBroadcastService, MsalRedirectComponent } from "@azure/msal-angular";
+import { PublicClientApplication, InteractionType, BrowserCacheLocation } from "@azure/msal-browser";
+
+import { environment } from 'src/environments/environment';
+
+const isIE =
+  window.navigator.userAgent.indexOf("MSIE ") > -1 ||
+  window.navigator.userAgent.indexOf("Trident/") > -1;
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,10 +23,45 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MsalModule.forRoot( new PublicClientApplication({ // MSAL Configuration
+      auth: {
+          clientId: environment.clientId,
+          authority: `https://login.microsoftonline.com/${environment.tenantId}`,
+          redirectUri: "http://localhost:4200",
+      },
+      cache: {
+          cacheLocation : BrowserCacheLocation.LocalStorage,
+          storeAuthStateInCookie: true, // set to true for IE 11
+      },
+      system: {
+          loggerOptions: {
+              loggerCallback: () => {},
+              piiLoggingEnabled: isIE
+          }
+      }
+  }), {
+      interactionType: InteractionType.Redirect, // MSAL Guard Configuration
+  }, {
+      interactionType: InteractionType.Redirect, // MSAL Interceptor Configuration
+      protectedResourceMap: new Map([
+          ['https://graph.microsoft.com/v1.0/me', ['user.read']],
+          ['https://api.myapplication.com/users/*', ['customscope.read']],
+          ['http://localhost:4200/about/', null] 
+      ])
+  })
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
