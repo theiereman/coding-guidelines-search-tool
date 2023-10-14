f361c9dff1ff3a3d19459aeeba59503972c735ff
Thomas EYERMANN <thomas.eyermann@custy.com>
2023-10-14 09:19:29 +0200

    requete graph pour récupérer le nom d'un worksheet

---
+++ src/app/constants/graph-api.constants.ts
@@ -0,0 +1,5 @@
+export const GRAPH_API_IDS = {
+    siteId: 'custyburrus.sharepoint.com,63756e78-963f-4d88-b223-d59fbe37f706,071b9c7a-e181-4a2e-a512-0a1fa7611824',
+    driveId:'b!eG51Yz-WiE2yI9Wfvjf3BnqcGweB4S5KpRIKH6dhGCQ-kgyuDTQLQry6tNoAkiUJ',
+    codingGuidelineWorkbookItemId:'01LP6ZGN6CGSAE7KWF65FYPI5BL5VIK472'
+}
--- src/app/microsoft-graph.service.ts
+++ src/app/microsoft-graph.service.ts
@@ -1,18 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Client } from '@microsoft/microsoft-graph-client';
+import { AuthService } from './auth.service';
+import { Observable, catchError, from, map, of } from 'rxjs';
+import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
+
+import { GRAPH_API_IDS } from './constants/graph-api.constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MicrosoftGraphService {
+  constructor(
+    private authService: AuthService
+  ) {}
 
-  private graphClient?: Client;
+    //TODO : trouver comment ne pas faire requête en boucle quand erreur
 
-  constructor() {
+  getWorksheets():Observable<string[] | undefined> {
+    if (!this.authService.graphClient) {
+      console.error('Graph service uninitialized')
+      return of(undefined);
+    }
 
-    // this.graphClient = Client.init(
+    //GET /sites/custyburrus.sharepoint.com,63756e78-963f-4d88-b223-d59fbe37f706,071b9c7a-e181-4a2e-a512-0a1fa7611824
+    //      /drives/b!eG51Yz-WiE2yI9Wfvjf3BnqcGweB4S5KpRIKH6dhGCQ-kgyuDTQLQry6tNoAkiUJ
+    //        /items/01LP6ZGN6CGSAE7KWF65FYPI5BL5VIK472/workbook/worksheets
 
-    // )
+    let worksheetNames:Observable<string[]> = from(this.authService.graphClient
+      .api(`/sites/${GRAPH_API_IDS.siteId}/drives/${GRAPH_API_IDS.driveId}/items/${GRAPH_API_IDS.codingGuidelineWorkbookItemId}/workbook/worksheets`)
+      .get()).pipe(
+        map((res) => {
+          //collection de MicrosoftGraph.WorkbookWorksheet
+          return res.value.map((worksheet:MicrosoftGraph.WorkbookWorksheet) => {
+            return worksheet.name as string;
+          })
+        }),
+        catchError((err, caught) => {
+          console.error(err);
+          return caught;
+        })
+      );
 
-   }
+    return worksheetNames;
+  } 
 }
