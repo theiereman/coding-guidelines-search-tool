35b65e4f7539d3e1cc99fd39c9acb28252258f9d
Thomas Eyermann <thomas.eyermann@protonmail.com>
2023-10-29 17:52:31 +0100

    ajout d'un module pour les alertes

--- src/app/components/alerts/alerts.component.ts
+++ src/app/components/alerts/alerts.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertsService } from '../../alerts.service';
 import { Alert } from '../../alert';
+import { trigger,query, transition, style, stagger, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.component.html',
-  styleUrls: ['./alerts.component.css']
+  styleUrls: ['./alerts.component.css'],
+  animations: [
+    trigger('listInOut', [
+      transition(':enter', [
+        query('*', [
+          style({ opacity: 0, transform: 'translateY(50%)' }),
+          stagger(100, [
+            animate('1s ease', style({ opacity: 1, transform: 'translateY(0)' }))
+          ])
+        ])
+      ]),
+      transition(':leave', [
+        query('*', [
+          style({ opacity: 1, transform: 'translateY(0)' }),
+          stagger(100, [
+            animate('.5s ease', style({ opacity: 0, transform: 'translateY(-50%)' }))
+          ])
+        ])
+      ])
+    ])],
 })
 export class AlertsComponent implements OnInit {
 
