d4d293590d5ea0f436f0fe358ad1653254ca8710
Thomas EYERMANN <thomas.eyermann@protonmail.com>
2023-11-06 10:19:27 +0100

    ajout item style pour alertes

--- src/app/components/alerts/alerts.component.ts
+++ src/app/components/alerts/alerts.component.ts
@@ -1,31 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertsService } from '../../services/alerts.service';
 import { IAlert } from '../../interfaces/ialert';
-import { trigger,query, transition, style, stagger, animate } from '@angular/animations';
+import { trigger,query, transition, style, stagger, animate, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.component.html',
-  styleUrls: ['./alerts.component.scss'],
+  styleUrls: [],
   animations: [
-    trigger('listInOut', [
-      transition(':enter', [
-        query('*', [
-          style({ opacity: 0, transform: 'translateY(50%)' }),
-          stagger(100, [
-            animate('1s ease', style({ opacity: 1, transform: 'translateY(0)' }))
-          ])
-        ])
+    trigger('list', [
+      transition("* => *", [query("@animate", stagger(100, animateChild()))]),
+    ]),
+    trigger('animate', [
+      transition(":enter", [
+        style({ opacity: 0, transform: "translateY(50%)" }),
+        animate(".5s ease", style({ opacity: 1, transform: "translateY(0))" }))
       ]),
       transition(':leave', [
-        query('*', [
-          style({ opacity: 1, transform: 'translateY(0)' }),
-          stagger(100, [
-            animate('.5s ease', style({ opacity: 0, transform: 'translateY(-50%)' }))
-          ])
-        ])
+        style({ opacity: 1, transform: 'translateY(0)' }),
+        animate('.5s ease', style({ opacity: 0, transform: 'translateY(-50%)' }))
       ])
-    ])],
+    ])
+  ],
 })
 export class AlertsComponent implements OnInit {
 
