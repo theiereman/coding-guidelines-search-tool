4337adf011c7a92cfa76d138aa550bb45c698e4e
Thomas EYERMANN <thomas.eyermann@protonmail.com>
2023-11-07 17:31:28 +0100

    généralisation de l'algo de normlisation des chaines

--- src/app/components/code-guideline-list/code-guideline-list.component.ts
+++ src/app/components/code-guideline-list/code-guideline-list.component.ts
@@ -33,24 +33,24 @@ export class CodeGuidelineListComponent {
   private filterCodingGuidelines(searchValue:string) {
     searchValue = customNormalization(searchValue)
 
-    if(searchValue === '') { this.filteredCodingGuidelinesItems = this.codingGuidelinesItems; return; }
+    //liste par défaut si recherche vide
+    if(searchValue === '') { 
+      this.filteredCodingGuidelinesItems = this.codingGuidelinesItems;
+      return; 
+    }
 
     this.filteredCodingGuidelinesItems = this.codingGuidelinesItems.filter(codingGuideline => {
-      //formattage de la recherche
-      searchValue.replace(/\s+/g, ' ');
-      searchValue = searchValue.toLowerCase();
-      
       let containsSearchedTerms = true;
 
-      //possibilité de chercher plusieurs termes séparés par un espace
+      //possibilité de chercher plusieurs termes séparés par un espace (ET logique entre les termes)
       let i = 0;
       let termsArray = searchValue.split(' ').filter(value => value.length > 0)
       while(containsSearchedTerms === true && i < termsArray.length) {
-        containsSearchedTerms =  customNormalization(codingGuideline.name.toLowerCase()).includes(termsArray[i])
-                              || customNormalization(codingGuideline.prefix.toLowerCase()).includes(termsArray[i]) 
-                              || customNormalization(codingGuideline.case.toLowerCase()).includes(termsArray[i]) 
-                              || customNormalization(codingGuideline.example?.toLowerCase() ?? "").includes(termsArray[i]) 
-                              || customNormalization(codingGuideline.sheetName.toLowerCase()).includes(termsArray[i]);
+        containsSearchedTerms =  customNormalization(codingGuideline.name).includes(termsArray[i])
+                              || customNormalization(codingGuideline.prefix).includes(termsArray[i]) 
+                              || customNormalization(codingGuideline.case).includes(termsArray[i]) 
+                              || customNormalization(codingGuideline.example ?? "").includes(termsArray[i]) 
+                              || customNormalization(codingGuideline.sheetName).includes(termsArray[i]);
         i++;
       }
       
