6845484659462b961f5e92a560085789e147350a
Thomas EYERMANN <thomas.eyermann@protonmail.com>
2023-11-07 17:25:08 +0100

    gestion des accents sur la recherche

---
+++ src/app/helpers/strings-normalizer.ts
@@ -0,0 +1,30 @@
+const accentsMap = new Map<string, string[]>([
+  ["A", ["Á", "À", "Ã", "Â", "Ä"]],
+  ["a", ["á", "à", "ã", "â", "ä"]],
+  ["E", ["É", "È", "Ê", "Ë"]],
+  ["e", ["é", "è", "ê", "ë"]],
+  ["I", ["Í", "Ì", "Î", "Ï"]],
+  ["i", ["í", "ì", "î", "ï"]],
+  ["O", ["Ó", "Ò", "Ô", "Õ", "Ö"]],
+  ["o", ["ó", "ò", "ô", "õ", "ö"]],
+  ["U", ["Ú", "Ù", "Û", "Ü"]],
+  ["u", ["ú", "ù", "û", "ü"]],
+  ["C", ["Ç"]],
+  ["c", ["ç"]],
+  ["N", ["Ñ"]],
+  ["n", ["ñ"]],
+]);
+
+const reducer = (acc: string, [key]: [string, string[]]) => {
+  const values = accentsMap.get(key);
+  if (values) {
+    const regexPattern = new RegExp(`[${values.join('')}]`, "g");
+    return acc.replace(regexPattern, key);
+  }
+  return acc;
+};
+
+const removeAccents = (text: string) => [...accentsMap].reduce(reducer, text);
+
+export const customNormalization = (text: string) => removeAccents(text.trim());
+  
