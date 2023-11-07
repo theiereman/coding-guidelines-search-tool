f118968eca7f87e06504e119184040cb05e9f801
Thomas EYERMANN <thomas.eyermann@protonmail.com>
2023-11-07 16:22:41 +0100

    mise à jour du scss pour le composant indiquant une connection requise

--- src/app/components/connection-required/connection-required.component.ts
+++ src/app/components/connection-required/connection-required.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-connection-required',
   templateUrl: './connection-required.component.html',
-  styleUrls: ['./connection-required.component.scss']
+  styleUrls: []
 })
 export class ConnectionRequiredComponent {
   constructor(private authService: AuthService) {}
