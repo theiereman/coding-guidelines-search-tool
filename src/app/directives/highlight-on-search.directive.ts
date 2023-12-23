56d658dc232d65e2405c722a49c54a41d00b5a04
Thomas Eyermann <thomas.eyermann@protonmail.com>
2023-12-23 18:33:18 +0100

    ajout d'un footer + modification de l'arbo scss

--- src/app/directives/highlight-on-search.directive.ts
+++ src/app/directives/highlight-on-search.directive.ts
@@ -1,5 +1,5 @@
 import { Directive, ElementRef, Input } from '@angular/core';
-import { customNormalization } from './helpers/strings-helper';
+import { customNormalization } from '../helpers/strings-helper';
 
 @Directive({
   selector: '[appHighlightOnSearch]'
