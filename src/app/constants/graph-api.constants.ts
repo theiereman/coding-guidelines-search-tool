b2450c6d4dd1e3a5efa3a2a0f5cc7487141a1db6
Thomas EYERMANN <thomas.eyermann@protonmail.com>
2024-02-02 14:28:07 +0100

    ajout des liens pour charte IHM et création issue de réintégration

--- src/app/constants/graph-api.constants.ts
+++ src/app/constants/graph-api.constants.ts
@@ -1,8 +1,15 @@
 export const GRAPH_API = {
-    siteId: 'custyburrus.sharepoint.com,63756e78-963f-4d88-b223-d59fbe37f706,071b9c7a-e181-4a2e-a512-0a1fa7611824',
-    driveId:'b!eG51Yz-WiE2yI9Wfvjf3BnqcGweB4S5KpRIKH6dhGCQ-kgyuDTQLQry6tNoAkiUJ',
-    codingGuidelineWorkbookItemId:'01LP6ZGN6CGSAE7KWF65FYPI5BL5VIK472',
-    clientId:'3abbff57-289e-42d7-a411-20c4be4924df',
-    tenantId:'ef66a229-8967-4ae5-b34e-759d70c2040d',
-    worksheetLink: 'https://custyburrus.sharepoint.com/:x:/s/GRP_equipe-windev/EcI0gE-qxfdLh6OhX2qFc_oBifrAzBu56JzCqWq3VCjxlA?e=ZfnZY6',
-}
+  siteId:
+    'custyburrus.sharepoint.com,63756e78-963f-4d88-b223-d59fbe37f706,071b9c7a-e181-4a2e-a512-0a1fa7611824',
+  driveId: 'b!eG51Yz-WiE2yI9Wfvjf3BnqcGweB4S5KpRIKH6dhGCQ-kgyuDTQLQry6tNoAkiUJ',
+  codingGuidelineWorkbookItemId: '01LP6ZGN6CGSAE7KWF65FYPI5BL5VIK472',
+  clientId: '3abbff57-289e-42d7-a411-20c4be4924df',
+  tenantId: 'ef66a229-8967-4ae5-b34e-759d70c2040d',
+
+  worksheetLink:
+    'https://custyburrus.sharepoint.com/:x:/s/GRP_equipe-windev/EcI0gE-qxfdLh6OhX2qFc_oBifrAzBu56JzCqWq3VCjxlA?e=ZfnZY6',
+  revolutionGuidelinesLink:
+    'https://custyburrus.sharepoint.com/:w:/s/GRP_equipe-windev/EYVwvBD7NcROjEuTebh-S9MBEliLwGVFvdBp-lcX-_dCRw?e=aWwEW4',
+  newIssueTooltipLink:
+    'http://10.128.88.23:6969/reintegration/reintegration.html',
+};
