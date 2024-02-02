c5f3c1634717e4aa43e506b8bba118dfb2507e5b
Thomas EYERMANN <thomas.eyermann@protonmail.com>
2024-02-02 14:28:07 +0100

    ajout des liens pour charte IHM et création issue de réintégration

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,35 +6,43 @@ import { GRAPH_API } from 'src/app/constants/graph-api.constants';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class NavbarComponent {
-  user?:IUser;
+  user?: IUser;
   loginDisplay = false;
-  
+
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.setLoginDisplay();
-    this.authService.getUserObservable().subscribe(user => {
+    this.authService.getUserObservable().subscribe((user) => {
       this.user = user;
-      this.setLoginDisplay()
-    })
+      this.setLoginDisplay();
+    });
   }
 
   openSharepointWorksheet() {
-    window.open(GRAPH_API.worksheetLink)
+    window.open(GRAPH_API.worksheetLink);
+  }
+
+  openRevolutionGuidelines() {
+    window.open(GRAPH_API.revolutionGuidelinesLink);
+  }
+
+  openNewIssueTooltip() {
+    window.open(GRAPH_API.newIssueTooltipLink);
   }
 
   login(): void {
-    this.authService.login().subscribe(_ => {
-      this.setLoginDisplay()
+    this.authService.login().subscribe((_) => {
+      this.setLoginDisplay();
     });
   }
 
   logout() {
-    this.authService.logout().subscribe(_ => {
-      this.setLoginDisplay()
+    this.authService.logout().subscribe((_) => {
+      this.setLoginDisplay();
     });
   }
 
