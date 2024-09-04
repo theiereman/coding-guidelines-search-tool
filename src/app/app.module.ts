2485e7725fd3b5da8c98940f3dbb403f8c281990
theiereman <thomas.eyermann@protonmail.com>
2024-09-04 16:26:10 +0200

    ajout de la gestion du router angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AlertsComponent } from './components/alerts/alerts.component';
 
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { MsalModule, MsalService, MsalGuard, MsalInterceptor, MsalBroadcastService, MsalRedirectComponent } from "@azure/msal-angular";
-import { PublicClientApplication, InteractionType, BrowserCacheLocation } from "@azure/msal-browser";
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import {
+  MsalModule,
+  MsalService,
+  MsalGuard,
+  MsalInterceptor,
+  MsalBroadcastService,
+  MsalRedirectComponent,
+} from '@azure/msal-angular';
+import {
+  PublicClientApplication,
+  InteractionType,
+  BrowserCacheLocation,
+} from '@azure/msal-browser';
 
 import { CodeGuidelineListComponent } from './components/code-guideline-list/code-guideline-list.component';
 import { CodeGuidelineSearchComponent } from './components/code-guideline-search/code-guideline-search.component';
@@ -17,12 +28,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConnectionRequiredComponent } from './components/connection-required/connection-required.component';
 import { GRAPH_API } from './constants/graph-api.constants';
 import { HighlightOnSearchDirective } from './directives/highlight-on-search.directive';
-
+import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
 
 const isIE =
-  window.navigator.userAgent.indexOf("MSIE ") > -1 ||
-  window.navigator.userAgent.indexOf("Trident/") > -1;
-
+  window.navigator.userAgent.indexOf('MSIE ') > -1 ||
+  window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
@@ -31,48 +42,58 @@ const isIE =
     CodeGuidelineSearchComponent,
     ConnectionRequiredComponent,
     NavbarComponent,
-    HighlightOnSearchDirective
+    HighlightOnSearchDirective,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AlertsModule,
     BrowserAnimationsModule,
-    MsalModule.forRoot( new PublicClientApplication({ // MSAL Configuration
+    HttpClientModule,
+    MsalModule.forRoot(
+      new PublicClientApplication({
+        // MSAL Configuration
         auth: {
-            clientId: GRAPH_API.clientId,
-            authority: `https://login.microsoftonline.com/${GRAPH_API.tenantId}`,
+          clientId: GRAPH_API.clientId,
+          authority: `https://login.microsoftonline.com/${GRAPH_API.tenantId}`,
         },
         cache: {
-            cacheLocation : BrowserCacheLocation.LocalStorage,
-            storeAuthStateInCookie: true, // set to true for IE 11
+          cacheLocation: BrowserCacheLocation.LocalStorage,
+          storeAuthStateInCookie: true, // set to true for IE 11
         },
         system: {
-            loggerOptions: {
-                loggerCallback: () => {},
-                piiLoggingEnabled: isIE
-            }
-        }
-    }), {
+          loggerOptions: {
+            loggerCallback: () => {},
+            piiLoggingEnabled: isIE,
+          },
+        },
+      }),
+      {
         interactionType: InteractionType.Redirect, // MSAL Guard Configuration
-    }, {
+      },
+      {
         interactionType: InteractionType.Redirect, // MSAL Interceptor Configuration
         protectedResourceMap: new Map([
-            ['https://graph.microsoft.com/*', ['User.Read', 'Sites.Read.All', 'Files.Read.All']],
-        ])
-    })
+          [
+            'https://graph.microsoft.com/*',
+            ['User.Read', 'Sites.Read.All', 'Files.Read.All'],
+          ],
+        ]),
+      }
+    ),
   ],
   schemas: [],
   providers: [
+    provideRouter(routes),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: MsalInterceptor,
-      multi: true
+      multi: true,
     },
     MsalService,
     MsalGuard,
-    MsalBroadcastService
+    MsalBroadcastService,
   ],
-  bootstrap: [AppComponent,MsalRedirectComponent]
+  bootstrap: [AppComponent, MsalRedirectComponent],
 })
-export class AppModule { }
+export class AppModule {}
