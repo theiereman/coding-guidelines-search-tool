3a2b2dfce0b99734df7a7cd68b9602dae6ec6d21
theiereman <thomas.eyermann@protonmail.com>
2024-09-04 16:27:00 +0200

    format prettier

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
 import { MsalService, MsalBroadcastService } from '@azure/msal-angular';
-import {AuthenticationResult,InteractionStatus, PublicClientApplication, InteractionType} from '@azure/msal-browser';
+import {
+  AuthenticationResult,
+  InteractionStatus,
+  PublicClientApplication,
+  InteractionType,
+} from '@azure/msal-browser';
 import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
 import { AuthCodeMSALBrowserAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser';
 import { Client } from '@microsoft/microsoft-graph-client';
-import { Observable,Subject, tap, filter, takeUntil, of, from, map, switchMap, catchError} from 'rxjs';
+import {
+  Observable,
+  Subject,
+  tap,
+  filter,
+  takeUntil,
+  of,
+  from,
+  map,
+  switchMap,
+  catchError,
+} from 'rxjs';
 import { IUser } from '../interfaces/iuser';
 import { AlertsService } from './alerts.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   public graphClient?: Client;
@@ -21,16 +37,18 @@ export class AuthService {
     private msalBroadcastService: MsalBroadcastService,
     private alertService: AlertsService
   ) {
-    //vérifie le statut de interactions 
+    //vérifie le statut de interactions
     this.msalBroadcastService.inProgress$
       .pipe(
-        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        filter(
+          (status: InteractionStatus) => status === InteractionStatus.None
+        ),
         takeUntil(this._destroying$)
       )
       .subscribe(() => {
         this.checkAndSetActiveAccount();
         this.updateUser();
-      })
+      });
   }
 
   getUserObservable(): Observable<IUser | undefined> {
@@ -48,11 +66,10 @@ export class AuthService {
   }
 
   logout(): Observable<void> {
-    return this.msalService.logoutRedirect()
-    .pipe(
-      tap(_ => {
+    return this.msalService.logoutRedirect().pipe(
+      tap((_) => {
         this.graphClient = undefined;
-        this.user$.next(undefined); 
+        this.user$.next(undefined);
       })
     );
   }
@@ -65,15 +82,18 @@ export class AuthService {
     return this.msalService.handleRedirectObservable();
   }
 
-  private checkAndSetActiveAccount(){
+  private checkAndSetActiveAccount() {
     let activeAccount = this.msalService.instance.getActiveAccount();
 
-    if (!activeAccount && this.msalService.instance.getAllAccounts().length > 0) {
+    if (
+      !activeAccount &&
+      this.msalService.instance.getAllAccounts().length > 0
+    ) {
       let accounts = this.msalService.instance.getAllAccounts();
       this.msalService.instance.setActiveAccount(accounts[0]);
     }
   }
-  
+
   private updateUser(): void {
     if (!this.isAuthenticated()) {
       this.user$.next(undefined);
@@ -84,9 +104,15 @@ export class AuthService {
       this.msalService.instance as PublicClientApplication,
       {
         account: this.msalService.instance.getActiveAccount()!,
-        scopes: ['user.read', 'mailboxsettings.read', 'calendars.readwrite','Sites.Read.All', 'Files.Read.All'],
+        scopes: [
+          'user.read',
+          'mailboxsettings.read',
+          'calendars.readwrite',
+          'Sites.Read.All',
+          'Files.Read.All',
+        ],
         interactionType: InteractionType.Redirect,
-      },
+      }
     );
 
     // Initialize the Graph client
@@ -95,35 +121,44 @@ export class AuthService {
     });
 
     // Get the user from Graph (GET /me)
-    from(this.graphClient.api('/me').select('displayName,mail,mailboxSettings,userPrincipalName').get())
+    from(
+      this.graphClient
+        .api('/me')
+        .select('displayName,mail,mailboxSettings,userPrincipalName')
+        .get()
+    )
       .pipe(
         tap((res) => {
           this.user$.next({
-              displayName: res.displayName ?? '',
-              email: res.mail ?? res.userPrincipalName ?? ''
-            }); 
+            displayName: res.displayName ?? '',
+            email: res.mail ?? res.userPrincipalName ?? '',
+          });
         }),
         tap((user) => {
           //récupération de l'image de profil au passage
           from(this.graphClient!.api('/me/photo/$value').get())
-          .pipe(map((blob) => {
-            var reader = new FileReader();
-            reader.readAsDataURL(blob); 
-            reader.onloadend = () => {
-              user.profilePictureBase64 = reader.result;
-              this.user$.next({
-                ...user,
-                profilePictureBase64: reader.result
-              });
-            }
-          })).subscribe();
+            .pipe(
+              map((blob) => {
+                var reader = new FileReader();
+                reader.readAsDataURL(blob);
+                reader.onloadend = () => {
+                  user.profilePictureBase64 = reader.result;
+                  this.user$.next({
+                    ...user,
+                    profilePictureBase64: reader.result,
+                  });
+                };
+              })
+            )
+            .subscribe();
         }),
         catchError((err) => {
           console.error(err);
           this.alertService.addError(err);
           return of();
         })
-      ).subscribe();
+      )
+      .subscribe();
   }
 
   ngOnDestroy(): void {
