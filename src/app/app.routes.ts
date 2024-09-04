365667c949dd4c11ec38d90f3c32fe54bbc406e4
theiereman <thomas.eyermann@protonmail.com>
2024-09-04 16:26:10 +0200

    ajout de la gestion du router angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { CodeGuidelineListComponent } from './components/code-guideline-list/code-guideline-list.component';
+
+export const routes: Routes = [
+  { path: '', component: CodeGuidelineListComponent },
+];
