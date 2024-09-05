d10da37d4846f763c96cbf4dee8e5cd478f8340e
theiereman <thomas.eyermann@protonmail.com>
2024-09-05 14:57:50 +0200

    deplacement du get authenticated user dans gitlab auth service

--- src/app/services/gitlab.service.ts
+++ src/app/services/gitlab.service.ts
@@ -15,39 +15,4 @@ export class GitlabService {
     private alertService: AlertsService,
     private httpClient: HttpClient
   ) {}
-
-  getAuthenticatedUser(): Observable<IGitlabUser | undefined> {
-    if (!this.authService.isAuthenticated()) return of(undefined);
-
-    const userInfoUrl = `${environment.gitlab_api_base_uri}/user`;
-    return this.httpClient
-      .get(userInfoUrl, {
-        headers: {
-          Authorization: `Bearer ${this.authService.getAccessToken()}`,
-        },
-      })
-      .pipe(
-        map((res: any) => {
-          console.log(res);
-          const userInfo: IGitlabUser = {
-            id: res.id,
-            username: res.username,
-            name: res.name,
-            state: res.state,
-            locked: res.locked,
-            avatar_url: res.avatar_url,
-            web_url: res.web_url,
-          };
-          return userInfo;
-        }),
-        tap({
-          error: (err) => {
-            this.alertService.addError(err.message);
-          },
-        }),
-        catchError(() => {
-          return of(undefined);
-        })
-      );
-  }
 }
