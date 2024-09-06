483c395968908f0eb7fb8b867885b2faa91e95de
theiereman <thomas.eyermann@protonmail.com>
2024-09-06 17:20:15 +0200

    simplification du composant de recherche + renommage proc normalisation pour simplification

--- src/app/directives/highlight-on-search.directive.ts
+++ src/app/directives/highlight-on-search.directive.ts
@@ -1,35 +1,35 @@
 import { Directive, ElementRef, Input } from '@angular/core';
-import { customNormalization } from '../helpers/strings-helper';
+import { normalize } from '../helpers/strings-helper';
 
 @Directive({
-  selector: '[appHighlightOnSearch]'
+  selector: '[appHighlightOnSearch]',
 })
-
-export class HighlightOnSearchDirective{
-  
+export class HighlightOnSearchDirective {
   private elementValue: string = '';
   private previousValue: string = '';
-  
+
   @Input() set appHighlightOnSearch(value: string) {
-    this.previousValue = customNormalization(value);
+    this.previousValue = normalize(value);
     this.highlight();
   }
 
-  constructor(private el: ElementRef) {  }
+  constructor(private el: ElementRef) {}
 
   public ngAfterViewInit(): void {
-    this.elementValue = customNormalization(this.el.nativeElement.innerText)
+    this.elementValue = normalize(this.el.nativeElement.innerText);
     this.highlight();
   }
 
   private highlight() {
-    let termsArray = this.previousValue.split(' ').filter(value => value.length > 0)
+    let termsArray = this.previousValue
+      .split(' ')
+      .filter((value) => value.length > 0);
 
     this.el.nativeElement.style.fontWeight = 400; //reset du highlihght
-    termsArray.forEach(term => {
-      if(this.previousValue !== '' && this.elementValue.includes(term)) {
-        this.el.nativeElement.style.fontWeight = 900;
-      } 
+    termsArray.forEach((term) => {
+      if (this.previousValue !== '' && this.elementValue.includes(term)) {
+        this.el.nativeElement.style.fontWeight = 700;
+      }
     });
   }
 }
