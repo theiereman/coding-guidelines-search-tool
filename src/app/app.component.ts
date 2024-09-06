271d6022238346177f30ec5f1b01809cfe43821e
theiereman <thomas.eyermann@protonmail.com>
2024-09-06 15:04:35 +0200

    global base scss cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,25 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { IUser } from './interfaces/iuser';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [],
-  host: {class: 'app'}
+  host: { class: 'app' },
 })
 export class AppComponent {
   title = 'Charte de programmation - Outil de recherche';
-  user?:IUser;
-  contentLoaded:boolean = false;
+  user?: IUser;
+  contentLoaded: boolean = false;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.authService.handleRedirects().subscribe();
-    this.authService.getUserObservable().subscribe(user => {
+    this.authService.getUserObservable().subscribe((user) => {
       this.contentLoaded = true;
       this.user = user;
-    })
+    });
   }
 }
