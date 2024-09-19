3c38c0c06f0434d69458714985dd66b62bd35b73
Thomas Eyermann <thomas.eyermann@protonmail.com>
2024-09-19 19:40:38 +0200

    utilisation du router + simplification de la navbar pour routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { CodeGuidelineListComponent } from './components/office365/code-guideline-list/code-guideline-list.component';
+import { NewIssueComponent } from './components/gitlab/new-issue/new-issue.component';
 
 export const routes: Routes = [
   { path: '', component: CodeGuidelineListComponent },
+  { path: 'issue-reintegration', component: NewIssueComponent },
+  { path: '**', component: CodeGuidelineListComponent },
 ];
