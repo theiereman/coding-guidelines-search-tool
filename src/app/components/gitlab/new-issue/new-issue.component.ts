99b07ae129829b01bc60e7d680c49854c0fb5f2b
Thomas Eyermann <thomas.eyermann@protonmail.com>
2024-09-20 15:49:15 +0200

    mise en forme et création des premiers contrôles pour le formulaire de réintégration

--- src/app/components/gitlab/new-issue/new-issue.component.ts
+++ src/app/components/gitlab/new-issue/new-issue.component.ts
@@ -1,9 +1,64 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { map } from 'rxjs';
+import { IGitlabLabel } from 'src/app/interfaces/gitlab/igitlab-label';
+import { IGitlabMilestone } from 'src/app/interfaces/gitlab/igitlab-milestone';
+import { GitlabService } from 'src/app/services/gitlab.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-new-issue',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass, NgFor],
   templateUrl: './new-issue.component.html',
 })
-export class NewIssueComponent {}
+export class NewIssueComponent {
+  milestones: IGitlabMilestone[] = [];
+  labels: IGitlabLabel[] = [];
+
+  developmentType = new FormControl('');
+  isBugCorrection = new FormControl('');
+  isQuoiDeNeuf = new FormControl('');
+  title = new FormControl('');
+  description = new FormControl('');
+  milestone = new FormControl('');
+
+  constructor(private gitlabService: GitlabService) {}
+
+  ngOnInit(): void {
+    this.updateMilestoneList();
+    this.updateLabelList();
+  }
+
+  private updateLabelList() {
+    this.gitlabService
+      .getLabelsFromProject(environment.gitlab_id_projet_reintegration)
+      .pipe(
+        map((labels) => {
+          return labels.filter(
+            (label: IGitlabLabel) =>
+              label.name !== 'bug' && label.name !== 'quoi de neuf ?'
+          );
+        })
+      )
+      .subscribe((labels) => {
+        this.labels = labels;
+      });
+  }
+
+  private updateMilestoneList() {
+    this.gitlabService
+      .getMilestonesFromProject(environment.gitlab_id_projet_reintegration)
+      .subscribe((milestones) => {
+        this.milestones = milestones;
+        if (this.milestones.length === 0) {
+          this.milestones.push({
+            id: 0,
+            title: 'Aucune milestone',
+            description: '',
+          });
+        }
+      });
+  }
+}
