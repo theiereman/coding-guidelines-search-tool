1105c94bf0fafb71396798ef2e5f2b77b853d689
theiereman <thomas.eyermann@protonmail.com>
2024-09-24 15:00:32 +0200

    ajout d'une alerte success

--- src/app/components/alerts/alerts.component.ts
+++ src/app/components/alerts/alerts.component.ts
@@ -1,37 +1,49 @@
 import { Component, OnInit, Optional } from '@angular/core';
 import { AlertsService } from '../../services/alerts.service';
-import { IAlert } from '../../interfaces/ialert';
-import { trigger,query, transition, style, stagger, animate, animateChild } from '@angular/animations';
-import { NgFor } from '@angular/common';
+import { IAlert, AlertType } from '../../interfaces/ialert';
+import {
+  trigger,
+  query,
+  transition,
+  style,
+  stagger,
+  animate,
+  animateChild,
+} from '@angular/animations';
+import { NgClass, NgFor } from '@angular/common';
 
 @Component({
-    selector: 'app-alerts',
-    templateUrl: './alerts.component.html',
-    styleUrls: [],
-    animations: [
-        trigger('list', [
-            transition("* => *", [query("@animate", stagger(100, animateChild()), { optional: true })]),
-        ]),
-        trigger('animate', [
-            transition(":enter", [
-                style({ opacity: 0, transform: "translateY(50%)" }),
-                animate(".5s ease", style({ opacity: 1, transform: "translateY(0))" }))
-            ]),
-            transition(':leave', [
-                style({ opacity: 1, transform: 'translateY(0)' }),
-                animate('.5s ease', style({ opacity: 0, transform: 'translateY(-50%)' }))
-            ])
-        ])
-    ],
-    standalone: true,
-    imports: [NgFor],
+  selector: 'app-alerts',
+  templateUrl: './alerts.component.html',
+  styleUrls: [],
+  animations: [
+    trigger('list', [
+      transition('* => *', [
+        query('@animate', stagger(100, animateChild()), { optional: true }),
+      ]),
+    ]),
+    trigger('animate', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(50%)' }),
+        animate('.5s ease', style({ opacity: 1, transform: 'translateY(0))' })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, transform: 'translateY(0)' }),
+        animate(
+          '.5s ease',
+          style({ opacity: 0, transform: 'translateY(-50%)' })
+        ),
+      ]),
+    ]),
+  ],
+  standalone: true,
+  imports: [NgFor, NgClass],
 })
 export class AlertsComponent implements OnInit {
+  alertEnum = AlertType;
+  constructor(public alertsService: AlertsService) {}
 
-  constructor(public alertsService: AlertsService) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   close(alert: IAlert) {
     this.alertsService.remove(alert);
