aa10044fe4e5f7092557d0262a75acaecefc7318
theiereman <thomas.eyermann@protonmail.com>
2024-10-08 14:03:10 +0200

    création d'une page 404 not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { CodeGuidelineListComponent } from './components/office365/code-guideline-list/code-guideline-list.component';
 import { NewIssueComponent } from './components/gitlab/new-issue/new-issue.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: CodeGuidelineListComponent },
   { path: 'issue-reintegration', component: NewIssueComponent },
-  { path: '**', component: CodeGuidelineListComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
