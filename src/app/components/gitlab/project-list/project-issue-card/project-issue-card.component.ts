f08bff87ff83f6f3423c61dec54e8757f682e95c
theiereman <thomas.eyermann@protonmail.com>
2024-10-09 17:42:09 +0200

    meilleure visibilité du projet selectionné

--- src/app/components/gitlab/project-list/project-issue-card/project-issue-card.component.ts
+++ src/app/components/gitlab/project-list/project-issue-card/project-issue-card.component.ts
@@ -1,4 +1,4 @@
-import { NgClass, NgIf } from '@angular/common';
+import { CommonModule, NgClass, NgIf } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { IGitlabIssue } from 'src/app/interfaces/gitlab/igitlab-issue';
 import { GitlabService } from 'src/app/services/gitlab.service';
@@ -7,12 +7,13 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-project-issue-card',
   standalone: true,
-  imports: [NgClass, NgIf],
+  imports: [CommonModule],
   templateUrl: './project-issue-card.component.html',
 })
 export class ProjectIssueCardComponent {
   @Input() issue?: IGitlabIssue = undefined;
   @Input() hideDetails: boolean = false;
+  @Input() isSelected: boolean = false;
 
   constructor(public gitlabService: GitlabService) {}
 
