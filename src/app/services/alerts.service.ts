c86dcd52966889bbcb22c0e3643261fbe40caf2d
theiereman <thomas.eyermann@protonmail.com>
2024-10-11 17:02:58 +0200

    affichage des alerts plus longtemps

--- src/app/services/alerts.service.ts
+++ src/app/services/alerts.service.ts
@@ -2,29 +2,28 @@ import { Injectable } from '@angular/core';
 import { IAlert, AlertType } from '../interfaces/ialert';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AlertsService {
   alerts: IAlert[] = [];
 
   addSuccess(message: string, autoRemove: boolean = true) {
-    let newAlert = {message: message, type: AlertType.Success};
+    let newAlert = { message: message, type: AlertType.Success };
     this.alerts.push(newAlert);
-    if(autoRemove) {
+    if (autoRemove) {
       setTimeout(() => {
         this.remove(newAlert);
-      }, 3000);
+      }, 5000);
     }
   }
 
   addError(message: string, autoRemove: boolean = true) {
-    let newAlert = {message: message, type: AlertType.Error};
+    let newAlert = { message: message, type: AlertType.Error };
     this.alerts.push(newAlert);
-    if(autoRemove) {
+    if (autoRemove) {
       setTimeout(() => {
         this.remove(newAlert);
-      }, 3000);
+      }, 5000);
     }
   }
 
