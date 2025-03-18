32ce666ff86ed6e75edf9d5c504e5c2cd98218ec
theiereman <thomas.eyermann@protonmail.com>
2025-03-18 17:09:28 +0100

    déplacement des paramétrages MS graph dans les variables d'environnement

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -15,6 +15,12 @@ export const environment = {
   GITLAB_ID_PROJET_REINTEGRATION: ${process.env.GITLAB_ID_PROJET_REINTEGRATION || 0},
   GITLAB_ID_PROJET_SUIVI_GENERAL: ${process.env.GITLAB_ID_PROJET_SUIVI_GENERAL || 0},
   GITLAB_ID_PROJET_CORRECTIONS_DIVERSES: ${process.env.GITLAB_ID_PROJET_CORRECTIONS_DIVERSES || 0},
+  MS_CLIENT_ID: '${process.env.MS_CLIENT_ID || ""}',
+  MS_DRIVE_ID: '${process.env.MS_DRIVE_ID || ""}',
+  MS_SITE_ID: '${process.env.MS_SITE_ID || ""}',
+  MS_TENANT_ID: '${process.env.MS_TENANT_ID || ""}',
+  MS_WORKBOOK_ID: '${process.env.MS_WORKBOOK_ID || ""}',
+  MS_WORKSHEET_LINK: '${process.env.MS_WORKSHEET_LINK || ""}',
 };
 `;
 
